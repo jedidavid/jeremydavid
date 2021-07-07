718393ebe9534d2a0c501ab6f244d94683eadf2d
Jeremy David <jeremydb.david@gmail.com>
2021-07-07 15:15:52 +0400

    setup

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4130ed59e701abd06774c102fe1a8061'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2aa59666-ec8a-4862-89a4-5a13ef97f65f'
+};
