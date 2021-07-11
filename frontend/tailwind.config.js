1cd97c653b03f7a126e0d64e3838da8186b12bbc
Jedi David <jeremydb.david@gmail.com>
2021-07-11 22:25:59 +0400

    tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,32 @@
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1740px",
+    },
+    extend: {
+      flex: {
+        sticky: "1 0 auto",
+      },
+      colors: {
+        primary: "#143cff",
+        "light-black": "#111111",
+      },
+    },
+    container: {
+      padding: "1rem",
+    },
+    fontFamily: {
+      heading: ["Karla", "sans-serif"],
+      body: ["Karla", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
