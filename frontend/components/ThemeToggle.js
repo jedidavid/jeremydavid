005749db79a736c146ab3aa7e1c6393e6ba5bbbc
Jedi David <jeremydb.david@gmail.com>
2021-07-11 22:29:42 +0400

    theme toggle

---
+++ frontend/components/ThemeToggle.js
@@ -0,0 +1,23 @@
+import { useTheme } from "next-themes";
+import { HiMoon, HiSun } from "react-icons/hi";
+
+const ThemeToggle = () => {
+  const { theme, setTheme } = useTheme();
+  return (
+    <div className="transition duration-500 ease-in-out rounded-full p-2">
+      {theme === "dark" ? (
+        <HiSun
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          className="text-black dark:text-white text-2xl cursor-pointer"
+        />
+      ) : (
+        <HiMoon
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          className="text-black dark:text-black text-2xl cursor-pointer"
+        />
+      )}
+    </div>
+  );
+};
+
+export default ThemeToggle;
