37ee01a69806009f796937d5d947ce42c1422cb0
Jedi David <jeremydb.david@gmail.com>
2021-07-25 21:26:15 +0400

    google font

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -11,13 +11,17 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
-        <body className="bg-white text-light-black dark:bg-light-black dark:text-white">
+        <body className="bg-white text-black dark:bg-black dark:text-white">
           <Main />
           <NextScript />
         </body>
