bbbad349a720757f6b1ee71e5327146b727bea90
Jedi David <jeremydb.david@gmail.com>
2021-07-25 21:25:29 +0400

    work component

---
+++ frontend/components/Works.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Card from "../components/Card";
+import Link from "next/link";
+
+const Works = ({ works }) => {
+  return (
+    <div>
+      <p className="sm:text-2xl text-xl font-medium mb-6">Featured Works</p>
+      <div className="grid lg:grid-cols-2 grid-cols-1 xl:gap-16 gap-8">
+        {works.map((work) => (
+          <div className="" key={work.id}>
+            <Card
+              slug={work.slug}
+              imgUrl={work.image}
+              imgAlt={work.image.alt}
+              title={work.title}
+              link={work.link}
+              github={work.github}
+              tags={work.tags}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="text-right">
+        <Link href="works">
+          <a className="sm:text-2xl text-xl font-medium base-transition text-transition">
+            &#8594; More
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
