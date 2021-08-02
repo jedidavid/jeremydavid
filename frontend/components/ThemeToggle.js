8563fb0b1814a3d22d33173ce329a2d38236974d
Jeremy David <jeremydb.david@gmail.com>
2021-08-02 15:19:41 +0400

    remove context / set default theme

--- frontend/components/ThemeToggle.js
+++ frontend/components/ThemeToggle.js
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { HiMoon, HiSun } from "react-icons/hi";
 
 const ThemeToggle = () => {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
   return (
     <div className="transition duration-500 ease-in-out rounded-full p-2">
       {theme === "dark" ? (
