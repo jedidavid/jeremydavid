98590a5dda743efaa2e096f786616de43e5cf2ca
Jedi David <jeremydb.david@gmail.com>
2021-08-14 21:41:55 +0400

    seo

--- frontend/components/Seo.js
+++ frontend/components/Seo.js
@@ -12,6 +12,14 @@ const Seo = (props) => {
           title: `Jeremy David | ${props.title}`,
           description: props.desc,
           site_name: "Jeremy David",
+          images: [
+            {
+              url: "https://admin.jeremydavid.dev/uploads/seo_ad854856cd.jpg",
+              width: 700,
+              height: 700,
+              alt: "",
+            },
+          ],
         }}
       />
     </>
